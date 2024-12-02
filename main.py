615de9d07663f47c3409944230ca901976b598a0
Denis <kripmrx@gmail.com>
2024-12-02 12:56:01 +0100

    light changes in main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, File, UploadFile
 
 from utils import get_dialog_as_text
@@ -8,9 +9,7 @@ app = FastAPI()
 @app.post("/asr")
 async def asr(file: UploadFile = File(...)) -> dict:
     res = await get_dialog_as_text(file)
-
     return res
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
